6f24997e30ffcf888ee66b90352e5bc8b2586527
Sujit Singh <sujitkumarsingh29@gmail.com>
2020-06-07 11:04:47 +0530

    patch

--- console.py
+++ console.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python
 
 import os
-import googleapiclient.discovery
-from google.oauth2 import service_account
 import getpass
 import json
 import requests
 import sys
-import urllib
+import urllib3
+from urllib.parse import quote
 
 import boto3
-from google.oauth2 import service_account
-import googleapiclient.discovery
 
 
 def assume_role(account_id, role_name, duration, external_id, mfa_arn, mfa_token):
     role_arn = "arn:aws:iam::" + account_id + ":role/" + role_name
     role_session_name = "AssumeRoleSession"
+    # Specify the service and region here
     client = boto3.client('sts', 'us-west-2')
 
     response = client.assume_role(RoleArn=role_arn,
@@ -37,7 +35,7 @@ def assume_role(account_id, role_name, duration, external_id, mfa_arn, mfa_token
 def generate_federation_request_parameters(credentials_json):
     request_parameters = "?Action=getSigninToken"
     request_parameters += "&SessionDuration=3600"
-    request_parameters += "&Session=" + urllib.quote_plus(credentials_json)
+    request_parameters += "&Session=" + quote(credentials_json)
     return request_parameters
 
 
@@ -53,7 +51,7 @@ def generate_signin_request_parameters(signin_token, issuer):
     request_parameters = "?Action=login"
     request_parameters += "&Issuer=" + issuer
     request_parameters += "&Destination=" + \
-        urllib.quote_plus("https://console.aws.amazon.com/")
+        quote("https://console.aws.amazon.com/")
     request_parameters += "&SigninToken=" + signin_token
     return request_parameters
 
@@ -82,57 +80,25 @@ def validate_input(argv):
 
 
 # --- Start of CLI parsing ---
-if (validate_input(sys.argv) != True):
-    print "console.py ACCOUNT_ID ROLE_NAME MFA_ARN"
-    sys.exit(1)
+# if (validate_input(sys.argv) != True):
+#     print("console.py ACCOUNT_ID ROLE_NAME MFA_ARN")
+#     sys.exit(1)
 
-cloud = sys.argv[1]  # Select cloud (AWS, GCLOUD, AZURE)
+account_id = "317360785572"   # Account id of target account
+role_name = "roleTest1"  # Name of role to assume in target account
 
-if (cloud == "AWS")
-account_id = sys.argv[2]  # Account id of target account
-role_name = sys.argv[3]  # Name of role to assume in target account
 # MFA users ARN. More info: http://docs.aws.amazon.com/IAM/latest/UserGuide/reference_identifiers.html
-mfa_arn = sys.argv[4]
+mfa_arn = "arn:aws:iam::317360785572:user/sujitTest1"
 
 duration = 3600  # 1 Hour
 issuer = "Example.org"  # issuer name for signin url
- external_id = getpass.getpass("External ID: ")
-  mfa_token = getpass.getpass("MFA Token: ")
+external_id = getpass.getpass("External ID: ")
+mfa_token = getpass.getpass("MFA Token: ")
 
-   try:
-        url = run(account_id, role_name, mfa_arn,
-                  duration, issuer, external_id, mfa_token)
-        print url
-    except Exception, e:
-        print("Received error:", e)
-        sys.exit(1)
-
-# For Google Cloud
-elif (cloud == "GCLOUD")
-    
-    GOOGLE_APPLICATION_CREDENTIALS = sys.argv[2]  # Account id of target account
-
-    # Get credentials
-    credentials = service_account.Credentials.from_service_account_file(
-        filename=os.environ[GOOGLE_APPLICATION_CREDENTIALS],
-        scopes=['https://www.googleapis.com/auth/cloud-platform'])
-
-    # Create the Cloud IAM service object
-    service = googleapiclient.discovery.build(
-        'iam', 'v1', credentials=credentials)
-
-    # Call the Cloud IAM Roles API
-    # If using pylint, disable weak-typing warnings
-    # pylint: disable=no-member
-    response = service.roles().list().execute()
-    roles = response['roles']
-
-    # Process the response
-    for role in roles:
-        print('Title: ' + role['title'])
-        print('Name: ' + role['name'])
-        if 'description' in role:
-            print('Description: ' + role['description'])
-        print('')
-else
-print "Incorrect Cloud Entry \n try: (AWS, GCLOUD, AZURE)"
+try:
+    url = run(account_id, role_name, mfa_arn,
+              duration, issuer, external_id, mfa_token)
+    print(url)
+except Exception as e:
+    print("Received error:", e)
+    sys.exit(1)
---
+++ test.py
@@ -0,0 +1,5 @@
+#!/bin/env python
+
+import boto3
+
+s3 = boto3.resource('s3')
