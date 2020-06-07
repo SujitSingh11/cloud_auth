5bf4b353bccc4e289bce530e4aef1c02bd5f4656
Sujit Singh <sujitkumarsingh29@gmail.com>
2020-06-07 13:43:58 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,22 @@
 #!/bin/env python
 
 import boto3
+import json
 
-s3 = boto3.resource('s3')
+client = boto3.client('sts')
+
+role_arn="arn:aws:iam::317360785572:role/test-role"
+role_session_name = "test-session"
+duration = 3600
+
+response = client.assume_role(
+    RoleArn=role_arn,
+    RoleSessionName=role_session_name,
+    DurationSeconds=duration,
+)
+tmp_credentials = {
+        'sessionId': response['Credentials']['AccessKeyId'],
+        'sessionKey': response['Credentials']['SecretAccessKey'],
+        'sessionToken': response['Credentials']['SessionToken']
+}
+print(json.dumps(tmp_credentials))
