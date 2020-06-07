ca08b15e761ab5aec30a0ded9458e6690f3883fd
Sujit Singh <sujitkumarsingh29@gmail.com>
2020-06-07 15:27:09 +0530

    patch

--- test.py
+++ test.py
@@ -1,22 +1,39 @@
-#!/bin/env python
-
 import boto3
 import json
 
-client = boto3.client('sts')
-
-role_arn="arn:aws:iam::317360785572:role/test-role"
-role_session_name = "test-session"
-duration = 3600
-
-response = client.assume_role(
+def assume_role(role_arn, role_session_name, duration):
+    client_sts = boto3.client('sts')
+    response = client_sts.assume_role(
     RoleArn=role_arn,
     RoleSessionName=role_session_name,
     DurationSeconds=duration,
-)
-tmp_credentials = {
-        'sessionId': response['Credentials']['AccessKeyId'],
-        'sessionKey': response['Credentials']['SecretAccessKey'],
-        'sessionToken': response['Credentials']['SessionToken']
-}
-print(json.dumps(tmp_credentials))
+    )
+    tmp_credentials = {
+            'sessionId': response['Credentials']['AccessKeyId'],
+            'sessionKey': response['Credentials']['SecretAccessKey'],
+            'sessionToken': response['Credentials']['SessionToken']
+    }
+    return(json.dumps(tmp_credentials))
+
+def list_role():
+    i=1
+    client_iam = boto3.client('iam')
+    response = client_iam.list_roles(
+        MaxItems=100
+    )
+    for role in response['Roles']:
+        print(i," ",role['RoleName'])
+        i=i+1
+    return response
+
+
+print("\nSelect Role (i.e 1/2/3)")
+roles = list_role()
+selected_role = int(input("Select: "))
+role_session_name = str(input("Enter a Session Name (e.g Test-Session) : "))
+duration = 3600 # Duration 1 hr
+role_arn = roles['Roles'][selected_role-1]['Arn']
+
+print(assume_role(role_arn,role_session_name,duration)) # Assume Role
+
+print("\n Session has been Started for 1hr")
